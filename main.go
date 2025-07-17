4b65eee12fc5a4925ef6e7a9dba2c835868f1218
OscillatingBlock <aayusharma270506@gmail.com>
2025-07-17 23:48:34 +0530

    Fix call to main model

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"Drop-Key-TUI/tui"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	p := tea.NewProgram(tui.New(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
